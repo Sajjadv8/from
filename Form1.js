17d47800bf231b7e0de868a3c056aae444109a82
sajjad <radsjad9@gmail.com>
2022-09-13 16:40:52 +0430

    change status css

--- Form1.js
+++ Form1.js
@@ -22,7 +22,12 @@ $(function () {
 
         $.get("https://gorest.co.in/public/v2/users", function (data) {
             $.each(data, function (i, person) {
-                $(".tbody").append("<tr><td>" + person.name + "</td>" + "<td>" + person.email + "</td>" + "<td>" + person.gender + "</td>" + "<td>" + person.status + "</td>" + "<td>" + "<button type='button' aria-label='close' class='closeclick close'><span aria-hidden='true'>X</span></button> " + "</td></tr>");
+
+                if (person.status == "active") {
+                    $(".tbody").append("<tr class='active'><td>" + person.name + "</td>" + "<td>" + person.email + "</td>" + "<td>" + person.gender + "</td>" + "<td>" + person.status + "</td>" + "<td>" + "<button type='button' aria-label='close' class='closeclick close'><span aria-hidden='true'>X</span></button> " + "</td></tr>");
+                } else {
+                    $(".tbody").append("<tr class='inactive'><td>" + person.name + "</td>" + "<td>" + person.email + "</td>" + "<td>" + person.gender + "</td>" + "<td>" + person.status + "</td>" + "<td>" + "<button type='button' aria-label='close' class='closeclick close'><span aria-hidden='true'>X</span></button> " + "</td></tr>");
+                }
 
             })
 
@@ -48,10 +53,10 @@ $("#send-Data").on("click", function () {
     $("tbody").append(tr);
 
     var data = {
-        name: "Ali1",
-        email: "Hosse1in@Ali.com",
-        gender: "male",
-        status: "active"
+        name: "",
+        email: "",
+        gender: "",
+        status: ""
     }
 
     // $.post("https://gorest.co.in/public/v2/users",data, function (data) {
@@ -61,7 +66,7 @@ $("#send-Data").on("click", function () {
     $.ajax({
         type: "POST",
         headers: {
-            'Authorization':'Bearer 86e29a96bbbddfddf032fab39ba6ef8f0895beefbe5bae18586ac6ae4d203b55'
+            'Authorization': 'Bearer 86e29a96bbbddfddf032fab39ba6ef8f0895beefbe5bae18586ac6ae4d203b55'
         },
         url: "https://gorest.co.in/public/v2/users",
         data: data,
@@ -69,6 +74,7 @@ $("#send-Data").on("click", function () {
             alert(response.id);
         }
     });
+
 
 
 
