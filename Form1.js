2690a703381e8ec7e32276f78577f62d22124548
sajjad <radsjad9@gmail.com>
2022-09-14 13:36:54 +0430

    CHANGE DOocument js

--- Form1.js
+++ Form1.js
@@ -1,4 +1,37 @@
 // add book to list
+$(document).on("click", ".closeclick", function () {
+    $(this).parent().parent().remove();
+});
+
+$("#send-Data").on("click", function () {
+
+    var tr = $("<tr></tr>")
+        .append("<td>" + $("#name").val() + "</td>")
+        .append("<td>" + $("#email").val() + "</td>")
+        .append("<td>" + $("#gender").val() + "</td>")
+        .append("<td>" + $("#status").val() + "</td>")
+        .append("<td><button type='button'  aria-label='close' class='closeclick close'><span aria-hidden='true'>X</span></button></td>");
+
+    $("tbody").append(tr);
+
+    var data = {
+        name: "ali",
+        email: "redsahad@gmail.com",
+        gender: "male",
+        status: "active"
+    }
+    $.ajax({
+        type: "POST",
+        headers: {
+            'Authorization': 'Bearer 86e29a96bbbddfddf032fab39ba6ef8f0895beefbe5bae18586ac6ae4d203b55'
+        },
+        url: "https://gorest.co.in/public/v2/users",
+        data: data,
+        success: function (response) {
+            alert(response.id);
+        }
+    });
+});
 $(function () {
 
 
@@ -21,43 +54,3 @@ $(function () {
     })
 
 });
-
-$(document).on("click", ".closeclick", function () {
-    $(this).parent().parent().remove();
-});
-
-$("#send-Data").on("click", function () {
-
-    var tr = $("<tr></tr>")
-        .append("<td>" + $("#name").val() + "</td>")
-        .append("<td>" + $("#email").val() + "</td>")
-        .append("<td>" + $("#gender").val() + "</td>")
-        .append("<td>" + $("#status").val() + "</td>")
-        .append("<td><button type='button'  aria-label='close' class='closeclick close'><span aria-hidden='true'>X</span></button></td>");
-
-    $("tbody").append(tr);
-
-    var data = {
-        name: "ali",
-        email: "redsahad@gmail.com",
-        gender: "male",
-        status: "active"
-    }
-
-
-
-    $.ajax({
-        type: "POST",
-        headers: {
-            'Authorization': 'Bearer 86e29a96bbbddfddf032fab39ba6ef8f0895beefbe5bae18586ac6ae4d203b55'
-        },
-        url: "https://gorest.co.in/public/v2/users",
-        data: data,
-        success: function (response) {
-            alert(response.id);
-        }
-    });
-
-
-});
-
