c015b7e992c3a650e313fec7cbcf294c86fa9d20
sajjad <radsjad9@gmail.com>
2022-09-19 08:41:43 +0430

    change

--- Form1.js
+++ Form1.js
@@ -12,10 +12,8 @@ $("#send-Data").on("click", function (e) {
         .append("<td><button type='button'  aria-label='close' class='closeclick close'><span aria-hidden='true'>X</span></button></td>");
 
     $("tbody").append(tr);
-    let rad = new XMLHttpRequest();
 
-    rad.open("Delete" , "https://gorest.co.in/public/v2/users");
-    
+
     var data = {
         name: $("#name").val(),
         email: $("#email").val(),
@@ -34,12 +32,12 @@ $("#send-Data").on("click", function (e) {
         }
     });
 
-   });
+});
 $(function () {
     $("#request-data").on("click", function () {
         $.get("https://gorest.co.in/public/v2/users", function (data) {
             $.each(data, function (i, person) {
-                
+
                 if (person.status == "active") {
                     $(".tbody").append("<tr class='active'><td>" + person.name + "</td>" + "<td>" + person.email + "</td>" + "<td>" + person.gender + "</td>" + "<td>" + person.status + "</td>" + "<td>" + "<button type='button' aria-label='close' class='closeclick close'><span aria-hidden='true'>X</span></button> " + "</td></tr>");
                 } else {
